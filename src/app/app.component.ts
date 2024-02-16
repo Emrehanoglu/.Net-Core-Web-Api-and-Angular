999abf41a31d0461c60841d18b59214efb7e6ff0
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-02-16 23:52:48 +0300

    ClientApp #5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'SocialApp';
+
+  categoryName = 'Telefon'
+
+  products = [
+    {id:1,name:'Samsung S5',price: 5000,isActive:false},
+    {id:2,name:'Samsung S6',price: 6000,isActive:true},
+    {id:3,name:'Samsung S7',price: 7000,isActive:false},
+    {id:4,name:'Samsung S8',price: 8000,isActive:false},
+    {id:5,name:'Samsung S9',price: 9000,isActive:true}
+  ];
 }
